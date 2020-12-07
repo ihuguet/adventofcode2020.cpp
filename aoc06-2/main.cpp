d4d3ff37255f0bb660f71dfa570e8fd36103080b
Iñigo Huguet <inigohuguet@hotmail.com>
2020-12-07 13:08:30 +0100

    Day 6 optimization: find_first_not_of instead of count

--- aoc06-2/main.cpp
+++ aoc06-2/main.cpp
@@ -2,23 +2,27 @@
 #include <string>
 #include <regex>
 
-static int read_next_group (std::istream &input, std::string &group_str);
+static constexpr bool validate_no_duplicate_rsps = true;
+
+static size_t read_next_group (std::istream &input, std::string &group_str);
 
 int main() {
     std::istream &input = std::cin;
     unsigned int yes_cnt = 0;
 
-    int num_persons;
+    size_t num_persons;
     std::string group_str;
     while (0 != (num_persons = read_next_group(input, group_str))) {
         std::sort(group_str.begin(), group_str.end());
 
-        auto ch_it = group_str.cbegin();
-        while (ch_it != group_str.cend()) {
-            auto ch_cnt = std::count(ch_it, group_str.cend(), *ch_it);
-            if (ch_cnt == num_persons)
+        size_t pos = 0;
+        while (pos < group_str.length()) {
+            size_t pos_end = group_str.find_first_not_of(group_str[pos], pos);
+            if (pos_end == std::string::npos)
+                pos_end = group_str.length();
+            if (pos_end - pos == num_persons)
                 yes_cnt++;
-            ch_it += ch_cnt;
+            pos = pos_end;
         }
     }
 
@@ -26,20 +30,20 @@ int main() {
     return 0;
 }
 
-static int read_next_group (std::istream &input, std::string &group_str) {
+static size_t read_next_group (std::istream &input, std::string &group_str) {
     static std::regex blankregex("^\\s*$");
 
-    int num_persons = 0;
+    size_t num_persons = 0;
     group_str.clear();
 
     std::string line;
     while (std::getline(input, line) && !std::regex_match(line, blankregex)) {
-        // avoid possible duplication in 1 person answer
-        std::sort(line.begin(), line.end());
-        auto last = std::unique(line.begin(), line.end());
-        line.erase(last, line.end());
+        if constexpr (validate_no_duplicate_rsps) {
+            std::sort(line.begin(), line.end());
+            auto last = std::unique(line.begin(), line.end());
+            line.erase(last, line.end());
+        }
 
-        // add line to group string
         group_str += line;
         num_persons++;
     }
